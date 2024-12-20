3252693a554db9aa7d570a04fa2f7443d98ab7ce
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-12-20 01:15:06 +0000

    update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@
 function getRemainingTime() {
     const now = new Date();
     const matchTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 0, 0);
-    
+
     if (now > matchTime) {
         matchTime.setDate(matchTime.getDate() + 1);
     }
-    
+
     const diff = matchTime - now;
 
     const days = Math.floor(diff / (1000 * 60 * 60 * 24))
     const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
     const seconds = Math.floor((diff % (1000 * 60)) / 1000)
-  
-    console.log({ dif, now, matchTime, days, hours, minutes, seconds })
-    
+
+    console.log({ diff, now, matchTime, days, hours, minutes, seconds })
+
     return {
         days,
         hours,
@@ -41,17 +41,17 @@ function updateTimer() {
     const formatedSeconds = seconds.toString().padStart(2, '0')
 
     console.log({ formatedDays, formatedHours, formatedMinutes, formatedSeconds })
-    
+
     if (Number(formatedDays) === 0) {
         timer.textContent = `${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
     }
-    
+
     if (Number(formatedDays) === 0 && Number(formatedHours) === 0) {
         timer.textContent = `${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
     }
-    
+
     timer.textContent = `${formatedDays} dias ${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
-    
+
 }
 
 // Update the timer every second
