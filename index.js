bcb66c419ab027ebcfa307147ad99de24b82f0b7
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-12-20 02:12:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 /**
  * Calculates the remaining time until the match starts.
+ * @param {string | Date} [targetDate]
  * @typedef {{ days: string, hours: string, minutes: string, seconds: string }} RemainingTime
  * @returns {RemainingTime} An object containing days, hours, minutes, and seconds remaining.
  */
-function getRemainingTime() {
+function getRemainingTime(targetDate) {
+
+  if (!targetDate)
+    throw new Error('No targetDate provided. Please provide a date string or Date object')
+
+  const target = typeof targetDate === 'string' ? new Date(targetDate) : targetDate
   const now = new Date();
-  const matchTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 0, 0);
-  if (now > matchTime) matchTime.setDate(matchTime.getDate() + 1);
 
   // @ts-ignore
-  const diff = matchTime - now;
+  const diff = target - now
+
+  if (diff <= 0) {
+    return {
+      days: '00',
+      hours: '00',
+      minutes: '00',
+      seconds: '00'
+    }
+  }
 
   const days = (Math.floor(diff / (1000 * 60 * 60 * 24))).toString();
-  const hours = (Math.floor((diff / (1000 * 60 * 60)) % 24)).toString().padStart(2, '0');
-  const minutes = (Math.floor((diff / (1000 * 60)) % 60)).toString().padStart(2, '0');
-  const seconds = (Math.floor((diff / 1000) % 60)).toString().padStart(2, '0');
+  const hours = (Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).toString().padStart(2, '0');
+  const minutes = (Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))).toString().padStart(2, '0');
+  const seconds = (Math.floor((diff % (1000 * 60)) / 1000)).toString().padStart(2, '0');
 
   return {
     days,
@@ -32,8 +45,7 @@ function updateTimer() {
   const timer = document.querySelector('#timer');
   if (!timer) throw new Error('Timer not defined or not exixts');
 
-  const { days, hours, minutes, seconds } = getRemainingTime();
-
+  const { days, hours, minutes, seconds } = getRemainingTime('2024-12-21T21:00:00');
 
   if (Number(days) === 0) {
     timer.textContent = `${hours}:${minutes}:${seconds}`;
